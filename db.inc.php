2c1540de727cd086e72bb1b6af7ed6f1a76a986b
865251 <karthikchettiyar3@gmail.com>
2025-05-10 12:16:09 +0530

    Update db.inc.php

--- db.inc.php
+++ db.inc.php
@@ -3,5 +3,5 @@
 define('DB_SERVER', 'mydatabase.cxgcymmigrmu.us-east-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'Karthik$123');
-define('DB_DATABASE', 'your_database_name');
+define('DB_DATABASE', 'mysqldatabase');
 ?>
